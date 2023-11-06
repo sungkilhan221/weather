2df0042db1be60cb67506a842a22f80e17db97bd
Sung Kil Han <sungkilhan221@gmail.com>
2023-11-06 15:54:29 -0500

    panel mt-2

--- components/InformationPanel.tsx
+++ components/InformationPanel.tsx
@@ -15,7 +15,7 @@ function InformationPanel({ city, lat, long, results }: Props) {
     <div className="bg-gradient-to-br from-[#394F68] to-[#183B7E] text-white p-10">
       <div className="pb-5">
         <h1 className="text-6xl font-bold">{decodeURI(city)}</h1>
-        <p className="text-xs text-gray-400">
+        <p className="text-xs text-gray-400 mt-2">
           Long/Lat: {long}, {lat}
         </p>
       </div>
