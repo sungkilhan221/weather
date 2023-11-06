cf5c181ae2af38bfa48ed4b045215988eeab0fe1
Sung Kil Han <sungkilhan221@gmail.com>
2023-11-06 18:45:26 -0500

    content null

--- app/location/[city]/[lat]/[long]/page.tsx
+++ app/location/[city]/[lat]/[long]/page.tsx
@@ -67,9 +67,11 @@ async function WeatherPage({ params: { city, lat, long } }: Props) {
             </p>
           </div>
 
-          <div className="m-2 mb-10">
-            <CalloutCard message={content || "Something wrong!"} />
-          </div>
+          {content ? (
+            <div className="m-2 mb-10">
+              <CalloutCard message={content || "Something wrong!"} />
+            </div>
+          ) : null}
 
           <div className="grid grid-cols-1 xl:grid-cols-2 gap-5 m-2">
             <StatCard
