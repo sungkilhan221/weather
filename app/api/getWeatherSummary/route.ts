eb5c35b7b8081adfd6cc0bd5138683d1cc9c8501
Sung Kil Han <sungkilhan221@gmail.com>
2023-11-06 17:44:17 -0500

    route trycatch

--- app/api/getWeatherSummary/route.ts
+++ app/api/getWeatherSummary/route.ts
@@ -2,34 +2,43 @@ import { NextResponse } from "next/server";
 import openai from "@/openai";
 
 export async function POST(request: Request) {
-  const { weatherData } = await request.json();
+  try {
+    const { weatherData } = await request.json();
 
-  // ${JSON.stringify(weatherData)}
-  const response = await openai.chat.completions.create({
-    model: "gpt-3.5-turbo",
-    messages: [
-      {
-        role: "system",
-        content:
-          "Pretend you're a weather news presenter presenting LIVE on television. be energetic and full of charisma. Introduce yourself as Adelyn and say you are LIVE from the WeatherChannel Headquarters. State the city you are providing a summary for. Then give a summary of todays weather only. Make it easy for the viewer to understand and know what to do to prepare for those weather conditions such as wear SPF if the UV is high etc. use the uv_index data provided to provide UV advice. Provide a joke regarding the weather. Assume the data came from your team at the news office and not the user.",
-      },
-      {
-        role: "user",
-        content: `Hi there, can i get a summary of todays weather, use the following information to get the weather data: ${JSON.stringify(
-          weatherData
-        )}`,
-      },
-    ],
-    temperature: 1,
-    max_tokens: 256,
-    top_p: 1,
-    frequency_penalty: 0,
-    presence_penalty: 0,
-  });
+    if (!weatherData) {
+      return new NextResponse("Missing weatherData", { status: 400 });
+    }
 
-  const data = response.choices[0].message;
+    // ${JSON.stringify(weatherData)}
+    const response = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "system",
+          content:
+            "Pretend you're a weather news presenter presenting LIVE on television. be energetic and full of charisma. Introduce yourself as Adelyn and say you are LIVE from the WeatherChannel Headquarters. State the city you are providing a summary for. Then give a summary of todays weather only. Make it easy for the viewer to understand and know what to do to prepare for those weather conditions such as wear SPF if the UV is high etc. use the uv_index data provided to provide UV advice. Provide a joke regarding the weather. Assume the data came from your team at the news office and not the user.",
+        },
+        {
+          role: "user",
+          content: `Hi there, can i get a summary of todays weather, use the following information to get the weather data: ${JSON.stringify(
+            weatherData
+          )}`,
+        },
+      ],
+      temperature: 1,
+      max_tokens: 256,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+    });
 
-  // console.log(data);
+    const data = response.choices[0].message;
 
-  return NextResponse.json(data);
+    // console.log(data);
+
+    return NextResponse.json(data);
+  } catch (error) {
+    console.log("[OPENAI ERROR]", error);
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
 }
