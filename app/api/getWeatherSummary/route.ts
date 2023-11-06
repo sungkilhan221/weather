4de2a2ad4771da20438842bcd9eb3b3138aa4476
Sung Kil Han <sungkilhan221@gmail.com>
2023-11-06 15:27:55 -0500

    weather first

---
+++ app/api/getWeatherSummary/route.ts
@@ -0,0 +1,35 @@
+import { NextResponse } from "next/server";
+import openai from "@/openai";
+
+export async function POST(request: Request) {
+  const { weatherData } = await request.json();
+
+  // ${JSON.stringify(weatherData)}
+  const response = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: "system",
+        content:
+          "Pretend you're a weather news presenter presenting LIVE on television. be energetic and full of charisma. Introduce yourself as Andrew and say you are LIVE from the WeatherChannel Headquarters. State the city you are providing a summary for. Then give a summary of todays weather only. Make it easy for the viewer to understand and know what to do to prepare for those weather conditions such as wear SPF if the UV is high etc. use the uv_index data provided to provide UV advice. Provide a joke regarding the weather. Assume the data came from your team at the news office and not the user.",
+      },
+      {
+        role: "user",
+        content: `Hi there, can i get a summary of todays weather, use the following information to get the weather data: ${JSON.stringify(
+          weatherData
+        )}`,
+      },
+    ],
+    temperature: 1,
+    max_tokens: 256,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+  });
+
+  const data = response.choices[0].message;
+
+  console.log(data);
+
+  return NextResponse.json(data);
+}
---
+++ components/CityPicker.tsx
@@ -0,0 +1,195 @@
+"use client";
+
+import { Country, State, City } from "country-state-city";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import Select from "react-select";
+import { GlobeIcon } from "@heroicons/react/solid";
+
+type option = {
+  value: {
+    latitude: string;
+    longitude: string;
+    isoCode: string;
+  };
+  label: string;
+} | null;
+
+type cityOption = {
+  value: {
+    latitude: string;
+    longitude: string;
+    countryCode: string;
+    name: string;
+    stateCode: string;
+  };
+  label: string;
+} | null;
+
+type stateOption = {
+  value: {
+    latitude: string;
+    longitude: string;
+    countryCode: string;
+    name: string;
+    isoCode: string;
+  };
+  label: string;
+} | null;
+
+const options = Country.getAllCountries().map((country) => ({
+  value: {
+    latitude: country.latitude,
+    longitude: country.longitude,
+    isoCode: country.isoCode,
+  },
+  label: country.name,
+}));
+
+function CityPicker() {
+  const [selectedCountry, setSelectedCountry] = useState<option>(null);
+  const [selectedCity, setSelectedCity] = useState<cityOption>(null);
+  const [selectedState, setSelectedState] = useState<stateOption>(null);
+  const router = useRouter();
+
+  const handleSelectedCountry = (option: option) => {
+    setSelectedCountry(option);
+    setSelectedCity(null);
+  };
+
+  const handleSelectedState = (option: stateOption) => {
+    setSelectedState(option);
+    setSelectedCity(null);
+  };
+
+  const handleSelectedCity = (option: cityOption) => {
+    setSelectedCity(option);
+    router.push(
+      `/location/${option?.value.name}/${option?.value.latitude}/${option?.value.longitude}`
+    );
+  };
+
+  const isCountryIncluded = (isoCode: string | undefined) => {
+    const countries = [
+      // "AU",
+      // "AT",
+      // "BR",
+      // "DE",
+      // "IN",
+      // "MY",
+      // "MX",
+      // "FM",
+      // "MM",
+      // "NZ",
+      // "NG",
+      // "PW",
+      // "SS",
+      "US",
+    ];
+
+    if (countries.includes(isoCode!)) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  return (
+    <div className="space-y-4">
+      <div className="space-y-2">
+        <div className="flex items-center space-x-2 text-white/80">
+          <GlobeIcon className="h-5 w-5 text-white" />
+          <label htmlFor="country">Country</label>
+        </div>
+        <Select
+          className="text-black"
+          value={selectedCountry}
+          onChange={handleSelectedCountry}
+          options={options}
+        />
+      </div>
+
+      {selectedCountry &&
+        isCountryIncluded(selectedCountry?.value.isoCode) === false && (
+          <div className="space-y-2">
+            <div className="flex items-center space-x-2 text-white/80">
+              <GlobeIcon className="h-5 w-5 text-white" />
+              <label htmlFor="city">City</label>
+            </div>
+            <Select
+              className="text-black"
+              value={selectedCity}
+              onChange={handleSelectedCity}
+              options={City.getCitiesOfCountry(
+                selectedCountry.value.isoCode
+              )?.map((state) => ({
+                value: {
+                  latitude: state.latitude!,
+                  longitude: state.longitude!,
+                  countryCode: state.countryCode,
+                  name: state.name,
+                  stateCode: state.stateCode,
+                },
+                label: state.name,
+              }))}
+            />
+          </div>
+        )}
+
+      {isCountryIncluded(selectedCountry?.value.isoCode) && (
+        <div className="space-y-2">
+          <div className="flex items-center space-x-2 text-white/80">
+            <GlobeIcon className="h-5 w-5 text-white" />
+            <label htmlFor="state">State</label>
+          </div>
+          <Select
+            className="text-black"
+            value={selectedState}
+            onChange={handleSelectedState}
+            options={State.getStatesOfCountry(
+              selectedCountry?.value.isoCode
+            )?.map((state) => ({
+              value: {
+                latitude: state.latitude!,
+                longitude: state.longitude!,
+                countryCode: state.countryCode,
+                name: state.name,
+                isoCode: state.isoCode,
+              },
+              label: state.name,
+            }))}
+          />
+        </div>
+      )}
+
+      {selectedCountry?.value.isoCode === "US" && selectedState && (
+        <div className="space-y-2">
+          <div className="flex items-center space-x-2 text-white/80">
+            <GlobeIcon className="h-5 w-5 text-white" />
+            <label htmlFor="city">City</label>
+          </div>
+          <Select
+            className="text-black"
+            value={selectedCity}
+            onChange={handleSelectedCity}
+            options={City.getCitiesOfState(
+              selectedCountry.value.isoCode,
+              selectedState.value.isoCode
+            )?.map((state) => ({
+              value: {
+                latitude: state.latitude!,
+                longitude: state.longitude!,
+                countryCode: state.countryCode,
+                name: state.name,
+                stateCode: state.stateCode,
+              },
+              label: state.name,
+            }))}
+          />
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default CityPicker;
