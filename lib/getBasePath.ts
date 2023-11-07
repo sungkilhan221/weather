4615ceb3a227bb9f9bfee8ddc1b750c5a3c8d42c
Sung Kil Han <sungkilhan221@gmail.com>
2023-11-06 20:19:22 -0500

    vercel_url

--- lib/getBasePath.ts
+++ lib/getBasePath.ts
@@ -1,8 +1,12 @@
 const getBasePath = () => {
+  // let base_url =
+  //   process.env.NODE_ENV === "development"
+  //     ? "http://localhost:3000"
+  //     : `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
   let base_url =
     process.env.NODE_ENV === "development"
       ? "http://localhost:3000"
-      : `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
+      : `https://weather-umber-seven.vercel.app`;
 
   return base_url;
 };
