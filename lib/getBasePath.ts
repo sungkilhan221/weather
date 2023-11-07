34bce38d8aac9add84a1c1f39e0faaac8f44aa53
Sung Kil Han <sungkilhan221@gmail.com>
2023-11-06 20:34:28 -0500

    molla

--- lib/getBasePath.ts
+++ lib/getBasePath.ts
@@ -1,12 +1,8 @@
 const getBasePath = () => {
-  // let base_url =
-  //   process.env.NODE_ENV === "development"
-  //     ? "http://localhost:3000"
-  //     : `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
   let base_url =
     process.env.NODE_ENV === "development"
-      ? "http://localhost:3000"
-      : `https://weather-umber-seven.vercel.app`;
+      ? `http://localhost:3000`
+      : `https://${process.env.VERCEL_URL}`;
 
   return base_url;
 };
