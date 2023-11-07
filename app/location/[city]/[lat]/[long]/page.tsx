f50a6ff669a6aedbd8fd0417b49b6f35e0c170b0
Sung Kil Han <sungkilhan221@gmail.com>
2023-11-06 19:00:06 -0500

    try catch

--- app/location/[city]/[lat]/[long]/page.tsx
+++ app/location/[city]/[lat]/[long]/page.tsx
@@ -36,96 +36,119 @@ async function WeatherPage({ params: { city, lat, long } }: Props) {
 
   const dataToSend = cleanData(results, city);
 
-  const res = await fetch(`${getBasePath()}/api/getWeatherSummary`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      weatherData: dataToSend,
-    }),
-  });
+  // const res = await fetch(`${getBasePath()}/api/getWeatherSummary`, {
+  //   method: "POST",
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //   },
+  //   body: JSON.stringify({
+  //     weatherData: dataToSend,
+  //   }),
+  // });
 
-  const GPTdata = await res.json();
+  // const GPTdata = await res.json();
 
-  const { content } = GPTdata;
+  // const { content } = GPTdata;
 
-  if (!content) return null;
+  // try catch fetch post data
 
-  return (
-    <div className="flex flex-col min-h-screen md:flex-row">
-      <InformationPanel city={city} lat={lat} long={long} results={results} />
+  try {
+    const res = await fetch(`${getBasePath()}/api/getWeatherSummary`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        weatherData: dataToSend,
+      }),
+    });
 
-      <div className="flex-1 p-5 lg:p-10">
-        <div className="p-5">
-          <div className="pb-5">
-            <h2 className="text-xl font-bold">Todays Overview</h2>
-            <p className="text-sm text-gray-400">
-              Last Updated at:{" "}
-              {new Date(results.current_weather.time).toLocaleString()} (
-              {results.timezone})
-            </p>
-          </div>
+    const GPTdata = await res.json();
 
-          {content ? (
-            <div className="m-2 mb-10">
-              <CalloutCard message={content || "Something wrong!"} />
+    const { content } = GPTdata;
+
+    return (
+      <div className="flex flex-col min-h-screen md:flex-row">
+        <InformationPanel city={city} lat={lat} long={long} results={results} />
+
+        <div className="flex-1 p-5 lg:p-10">
+          <div className="p-5">
+            <div className="pb-5">
+              <h2 className="text-xl font-bold">Todays Overview</h2>
+              <p className="text-sm text-gray-400">
+                Last Updated at:{" "}
+                {new Date(results.current_weather.time).toLocaleString()} (
+                {results.timezone})
+              </p>
             </div>
-          ) : null}
 
-          <div className="grid grid-cols-1 xl:grid-cols-2 gap-5 m-2">
-            <StatCard
-              title="Maximum Temperature"
-              metric={`${results.daily.temperature_2m_max[0].toFixed(1)}°`}
-              color="yellow"
-            />
+            {content ? (
+              <div className="m-2 mb-10">
+                <CalloutCard message={content || "Something wrong!"} />
+              </div>
+            ) : null}
 
-            <StatCard
-              title="Minimum Temperature"
-              metric={`${results.daily.temperature_2m_min[0].toFixed(1)}°`}
-              color="green"
-            />
-
-            <div>
+            <div className="grid grid-cols-1 xl:grid-cols-2 gap-5 m-2">
               <StatCard
-                title="UV Index"
-                metric={`${results.daily.uv_index_max[0].toFixed(1)}`}
-                color="rose"
+                title="Maximum Temperature"
+                metric={`${results.daily.temperature_2m_max[0].toFixed(1)}°`}
+                color="yellow"
               />
-              {Number(results.daily.uv_index_max[0].toFixed(1)) > 5 && (
-                <CalloutCard
-                  message={"The UV is high today, remember to wear sunscreen!"}
-                  warning
+
+              <StatCard
+                title="Minimum Temperature"
+                metric={`${results.daily.temperature_2m_min[0].toFixed(1)}°`}
+                color="green"
+              />
+
+              <div>
+                <StatCard
+                  title="UV Index"
+                  metric={`${results.daily.uv_index_max[0].toFixed(1)}`}
+                  color="rose"
                 />
-              )}
-            </div>
+                {Number(results.daily.uv_index_max[0].toFixed(1)) > 5 && (
+                  <CalloutCard
+                    message={
+                      "The UV is high today, remember to wear sunscreen!"
+                    }
+                    warning
+                  />
+                )}
+              </div>
 
-            <div className="flex space-x-3">
-              <StatCard
-                title="Wind Speed"
-                metric={`${results.current_weather.windspeed.toFixed(1)}m/s`}
-                color="cyan"
-              />
+              <div className="flex space-x-3">
+                <StatCard
+                  title="Wind Speed"
+                  metric={`${results.current_weather.windspeed.toFixed(1)}m/s`}
+                  color="cyan"
+                />
 
-              <StatCard
-                title="Wind Direction"
-                metric={`${results.current_weather.winddirection.toFixed(1)}°`}
-                color="violet"
-              />
+                <StatCard
+                  title="Wind Direction"
+                  metric={`${results.current_weather.winddirection.toFixed(
+                    1
+                  )}°`}
+                  color="violet"
+                />
+              </div>
             </div>
           </div>
-        </div>
 
-        <hr className="mb-5" />
+          <hr className="mb-5" />
 
-        <div className="space-y-3">
-          <TempChart results={results} />
-          <RainChart results={results} />
-          <HumidityChart results={results} />
+          <div className="space-y-3">
+            <TempChart results={results} />
+            <RainChart results={results} />
+            <HumidityChart results={results} />
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
 }
 
 export default WeatherPage;
